34128367639fc2e0cca421f3ef02f2e56374d826
André Lucas <andre@ae-35.com>
2024-01-26 11:52:34 +0000

    Don't re-raise when unpacking errors

--- grpc_auth_client.py
+++ grpc_auth_client.py
@@ -47,18 +47,23 @@ def auth(stub: auth_pb2_grpc.AuthenticatorServiceStub, args):
     except grpc.RpcError as e:
         # Unpack the error.
         status = rpc_status.from_call(e)
-        logging.error(f"PC failed: grpc_code={status.code} message='{status.message}'")
-        for detail in status.details:
-            # Unpack the ANY if it's a specific type.
-            if detail.Is(auth_pb2.S3ErrorDetails.DESCRIPTOR):
-                s3_error = auth_pb2.S3ErrorDetails()
-                detail.Unpack(s3_error)
-                tstr = auth_pb2.S3ErrorDetails.Type.DESCRIPTOR.values_by_number[
-                    s3_error.type
-                ].name  # String form of the S3 error type.
-                logging.error(
-                    f"S3ErrorDetails: type={tstr} http_status_code={s3_error.http_status_code}"
-                )
+        if status is None:
+            logging.error(f"RPC failed: {e}")
+        else:
+            logging.error(
+                f"PC failed: error={e} code={status.code} message='{status.details}'"
+            )
+            for detail in status.details:
+                # Unpack the ANY if it's a specific type.
+                if detail.Is(auth_pb2.S3ErrorDetails.DESCRIPTOR):
+                    s3_error = auth_pb2.S3ErrorDetails()
+                    detail.Unpack(s3_error)
+                    tstr = auth_pb2.S3ErrorDetails.Type.DESCRIPTOR.values_by_number[
+                        s3_error.type
+                    ].name  # String form of the S3 error type.
+                    logging.error(
+                        f"S3ErrorDetails: type={tstr} http_status_code={s3_error.http_status_code}"
+                    )
 
         return False
 
@@ -66,12 +71,14 @@ def auth(stub: auth_pb2_grpc.AuthenticatorServiceStub, args):
 def main(argv):
     p = argparse.ArgumentParser(description="AuthService client")
     p.add_argument("command", help="command to run", choices=["auth"])
+    p.add_argument("--access-key-id", help="AWS_ACCESS_KEY_ID value")
+    p.add_argument("--authorization-header", help="Authorization: header contents")
+    p.add_argument("--method", help="HTTP method", default="GET")
+    p.add_argument("--string-to-sign-base64", help="stringToSign field")
+    p.add_argument("--uri", help="server uri (will override address and port!)")
+    p.add_argument("-a", "--address", help="server address", default="127.0.0.1")
     p.add_argument("-p", "--port", type=int, default=8002, help="server listen port")
     p.add_argument("-v", "--verbose", action="store_true")
-    p.add_argument("--string-to-sign-base64", help="stringToSign field")
-    p.add_argument("--authorization-header", help="Authorization: header contents")
-    p.add_argument("--access-key-id", help="AWS_ACCESS_KEY_ID value")
-    p.add_argument("--method", help="HTTP method", default="GET")
 
     args = p.parse_args(argv)
     if not args.command:
@@ -84,8 +91,9 @@ def main(argv):
         logging.basicConfig(level=logging.INFO)
 
     # Set up a channel first.
-    port = args.port
-    server_address = f"dns:127.0.0.1:{port}"
+    server_address = f"dns:{args.address}:{args.port}"
+    if args.uri:
+        server_address = args.uri
     logging.debug(f"using server_address {server_address}")
     success = False
 
