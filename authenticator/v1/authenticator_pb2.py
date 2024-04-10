a8fb64f26852da057ec872a5d49f425ffc8f6537
André Lucas <andre@ae-35.com>
2024-04-10 12:31:15 +0100

    Buf generated code for proto update

--- authenticator/v1/authenticator_pb2.py
+++ authenticator/v1/authenticator_pb2.py
@@ -14,7 +14,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n$authenticator/v1/authenticator.proto\x12\x10\x61uthenticator.v1\"\xae\x04\n\x0eS3ErrorDetails\x12\x39\n\x04type\x18\x01 \x01(\x0e\x32%.authenticator.v1.S3ErrorDetails.TypeR\x04type\x12(\n\x10http_status_code\x18\x02 \x01(\x05R\x0ehttpStatusCode\"\xb6\x03\n\x04Type\x12\x14\n\x10TYPE_UNSPECIFIED\x10\x00\x12\x16\n\x12TYPE_ACCESS_DENIED\x10\x01\x12\'\n#TYPE_AUTHORIZATION_HEADER_MALFORMED\x10\x02\x12\x16\n\x12TYPE_EXPIRED_TOKEN\x10\x03\x12\x17\n\x13TYPE_INTERNAL_ERROR\x10\x04\x12\x1e\n\x1aTYPE_INVALID_ACCESS_KEY_ID\x10\x05\x12\x18\n\x14TYPE_INVALID_REQUEST\x10\x06\x12\x19\n\x15TYPE_INVALID_SECURITY\x10\x07\x12\x16\n\x12TYPE_INVALID_TOKEN\x10\x08\x12\x14\n\x10TYPE_INVALID_URI\x10\t\x12\x1b\n\x17TYPE_METHOD_NOT_ALLOWED\x10\n\x12 \n\x1cTYPE_MISSING_SECURITY_HEADER\x10\x0b\x12 \n\x1cTYPE_REQUEST_TIME_TOO_SKEWED\x10\x0c\x12!\n\x1dTYPE_SIGNATURE_DOES_NOT_MATCH\x10\r\x12\x1f\n\x1bTYPE_TOKEN_REFRESH_REQUIRED\x10\x0e\"\x90\x07\n\x17\x41uthenticateRESTRequest\x12)\n\x0estring_to_sign\x18\x01 \x01(\tH\x00R\x0cstringToSign\x88\x01\x01\x12\x36\n\x14\x61uthorization_header\x18\x02 \x01(\tH\x01R\x13\x61uthorizationHeader\x88\x01\x01\x12U\n\x0bhttp_method\x18\x03 \x01(\x0e\x32\x34.authenticator.v1.AuthenticateRESTRequest.HTTPMethodR\nhttpMethod\x12$\n\x0b\x62ucket_name\x18\x04 \x01(\tH\x02R\nbucketName\x88\x01\x01\x12\"\n\nobject_key\x18\x05 \x01(\tH\x03R\tobjectKey\x88\x01\x01\x12i\n\x10query_parameters\x18\x06 \x03(\x0b\x32>.authenticator.v1.AuthenticateRESTRequest.QueryParametersEntryR\x0fqueryParameters\x12^\n\rx_amz_headers\x18\x07 \x03(\x0b\x32:.authenticator.v1.AuthenticateRESTRequest.XAmzHeadersEntryR\x0bxAmzHeaders\x12*\n\x0etransaction_id\x18\x08 \x01(\tH\x04R\rtransactionId\x88\x01\x01\x1a\x42\n\x14QueryParametersEntry\x12\x10\n\x03key\x18\x01 \x01(\tR\x03key\x12\x14\n\x05value\x18\x02 \x01(\tR\x05value:\x02\x38\x01\x1a>\n\x10XAmzHeadersEntry\x12\x10\n\x03key\x18\x01 \x01(\tR\x03key\x12\x14\n\x05value\x18\x02 \x01(\tR\x05value:\x02\x38\x01\"\x97\x01\n\nHTTPMethod\x12\x1b\n\x17HTTP_METHOD_UNSPECIFIED\x10\x00\x12\x13\n\x0fHTTP_METHOD_GET\x10\x01\x12\x14\n\x10HTTP_METHOD_HEAD\x10\x02\x12\x14\n\x10HTTP_METHOD_POST\x10\x03\x12\x13\n\x0fHTTP_METHOD_PUT\x10\x04\x12\x16\n\x12HTTP_METHOD_DELETE\x10\x05\x42\x11\n\x0f_string_to_signB\x17\n\x15_authorization_headerB\x0e\n\x0c_bucket_nameB\r\n\x0b_object_keyB\x11\n\x0f_transaction_id\"w\n\x18\x41uthenticateRESTResponse\x12\x17\n\x07user_id\x18\x01 \x01(\tR\x06userId\x12-\n\x10original_user_id\x18\x02 \x01(\tH\x00R\x0eoriginalUserId\x88\x01\x01\x42\x13\n\x11_original_user_id\"\xcc\x02\n\x1f\x41uthenticatePresignedURLRequest\x12$\n\x0estring_to_sign\x18\x01 \x01(\tR\x0cstringToSign\x12\x10\n\x03url\x18\x02 \x01(\tR\x03url\x12]\n\x0bhttp_method\x18\x03 \x01(\x0e\x32<.authenticator.v1.AuthenticatePresignedURLRequest.HTTPMethodR\nhttpMethod\x12*\n\x0etransaction_id\x18\x04 \x01(\tH\x00R\rtransactionId\x88\x01\x01\"S\n\nHTTPMethod\x12\x1b\n\x17HTTP_METHOD_UNSPECIFIED\x10\x00\x12\x13\n\x0fHTTP_METHOD_GET\x10\x01\x12\x13\n\x0fHTTP_METHOD_PUT\x10\x02\x42\x11\n\x0f_transaction_id\"\x7f\n AuthenticatePresignedURLResponse\x12\x17\n\x07user_id\x18\x01 \x01(\tR\x06userId\x12-\n\x10original_user_id\x18\x02 \x01(\tH\x00R\x0eoriginalUserId\x88\x01\x01\x42\x13\n\x11_original_user_id\"\x88\x01\n\x14GetSigningKeyRequest\x12\x31\n\x14\x61uthorization_header\x18\x01 \x01(\tR\x13\x61uthorizationHeader\x12*\n\x0etransaction_id\x18\x02 \x01(\tH\x00R\rtransactionId\x88\x01\x01\x42\x11\n\x0f_transaction_id\"8\n\x15GetSigningKeyResponse\x12\x1f\n\x0bsigning_key\x18\x01 \x01(\x0cR\nsigningKey2\xe7\x02\n\x14\x41uthenticatorService\x12i\n\x10\x41uthenticateREST\x12).authenticator.v1.AuthenticateRESTRequest\x1a*.authenticator.v1.AuthenticateRESTResponse\x12\x81\x01\n\x18\x41uthenticatePresignedURL\x12\x31.authenticator.v1.AuthenticatePresignedURLRequest\x1a\x32.authenticator.v1.AuthenticatePresignedURLResponse\x12`\n\rGetSigningKey\x12&.authenticator.v1.GetSigningKeyRequest\x1a\'.authenticator.v1.GetSigningKeyResponseB\xce\x01\n\x14\x63om.authenticator.v1B\x12\x41uthenticatorProtoP\x01ZAbits.linode.com/LinodeApi/obj-endpoint/gen/proto/authenticator/v1\xa2\x02\x03\x41XX\xaa\x02\x10\x41uthenticator.V1\xca\x02\x10\x41uthenticator\\V1\xe2\x02\x1c\x41uthenticator\\V1\\GPBMetadata\xea\x02\x11\x41uthenticator::V1b\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n$authenticator/v1/authenticator.proto\x12\x10\x61uthenticator.v1\"\xc7\x04\n\x0eS3ErrorDetails\x12\x39\n\x04type\x18\x01 \x01(\x0e\x32%.authenticator.v1.S3ErrorDetails.TypeR\x04type\x12(\n\x10http_status_code\x18\x02 \x01(\x05R\x0ehttpStatusCode\"\xcf\x03\n\x04Type\x12\x14\n\x10TYPE_UNSPECIFIED\x10\x00\x12\x16\n\x12TYPE_ACCESS_DENIED\x10\x01\x12\'\n#TYPE_AUTHORIZATION_HEADER_MALFORMED\x10\x02\x12\x16\n\x12TYPE_EXPIRED_TOKEN\x10\x03\x12\x17\n\x13TYPE_INTERNAL_ERROR\x10\x04\x12\x1e\n\x1aTYPE_INVALID_ACCESS_KEY_ID\x10\x05\x12\x18\n\x14TYPE_INVALID_REQUEST\x10\x06\x12\x19\n\x15TYPE_INVALID_SECURITY\x10\x07\x12\x16\n\x12TYPE_INVALID_TOKEN\x10\x08\x12\x14\n\x10TYPE_INVALID_URI\x10\t\x12\x1b\n\x17TYPE_METHOD_NOT_ALLOWED\x10\n\x12 \n\x1cTYPE_MISSING_SECURITY_HEADER\x10\x0b\x12 \n\x1cTYPE_REQUEST_TIME_TOO_SKEWED\x10\x0c\x12!\n\x1dTYPE_SIGNATURE_DOES_NOT_MATCH\x10\r\x12\x1f\n\x1bTYPE_TOKEN_REFRESH_REQUIRED\x10\x0e\x12\x17\n\x13TYPE_NO_SUCH_BUCKET\x10\x0f\"\x9b\t\n\x17\x41uthenticateRESTRequest\x12)\n\x0estring_to_sign\x18\x01 \x01(\tH\x00R\x0cstringToSign\x88\x01\x01\x12\x36\n\x14\x61uthorization_header\x18\x02 \x01(\tH\x01R\x13\x61uthorizationHeader\x88\x01\x01\x12U\n\x0bhttp_method\x18\x03 \x01(\x0e\x32\x34.authenticator.v1.AuthenticateRESTRequest.HTTPMethodR\nhttpMethod\x12$\n\x0b\x62ucket_name\x18\x04 \x01(\tH\x02R\nbucketName\x88\x01\x01\x12\"\n\nobject_key\x18\x05 \x01(\tH\x03R\tobjectKey\x88\x01\x01\x12i\n\x10query_parameters\x18\x06 \x03(\x0b\x32>.authenticator.v1.AuthenticateRESTRequest.QueryParametersEntryR\x0fqueryParameters\x12^\n\rx_amz_headers\x18\x07 \x03(\x0b\x32:.authenticator.v1.AuthenticateRESTRequest.XAmzHeadersEntryR\x0bxAmzHeaders\x12*\n\x0etransaction_id\x18\x08 \x01(\tH\x04R\rtransactionId\x88\x01\x01\x12g\n\x10x_akamai_headers\x18\t \x03(\x0b\x32=.authenticator.v1.AuthenticateRESTRequest.XAkamaiHeadersEntryR\x0exAkamaiHeaders\x12?\n\x19skip_timestamp_validation\x18\n \x01(\x08H\x05R\x17skipTimestampValidation\x88\x01\x01\x1a\x42\n\x14QueryParametersEntry\x12\x10\n\x03key\x18\x01 \x01(\tR\x03key\x12\x14\n\x05value\x18\x02 \x01(\tR\x05value:\x02\x38\x01\x1a>\n\x10XAmzHeadersEntry\x12\x10\n\x03key\x18\x01 \x01(\tR\x03key\x12\x14\n\x05value\x18\x02 \x01(\tR\x05value:\x02\x38\x01\x1a\x41\n\x13XAkamaiHeadersEntry\x12\x10\n\x03key\x18\x01 \x01(\tR\x03key\x12\x14\n\x05value\x18\x02 \x01(\tR\x05value:\x02\x38\x01\"\x97\x01\n\nHTTPMethod\x12\x1b\n\x17HTTP_METHOD_UNSPECIFIED\x10\x00\x12\x13\n\x0fHTTP_METHOD_GET\x10\x01\x12\x14\n\x10HTTP_METHOD_HEAD\x10\x02\x12\x14\n\x10HTTP_METHOD_POST\x10\x03\x12\x13\n\x0fHTTP_METHOD_PUT\x10\x04\x12\x16\n\x12HTTP_METHOD_DELETE\x10\x05\x42\x11\n\x0f_string_to_signB\x17\n\x15_authorization_headerB\x0e\n\x0c_bucket_nameB\r\n\x0b_object_keyB\x11\n\x0f_transaction_idB\x1c\n\x1a_skip_timestamp_validation\"w\n\x18\x41uthenticateRESTResponse\x12\x17\n\x07user_id\x18\x01 \x01(\tR\x06userId\x12-\n\x10original_user_id\x18\x02 \x01(\tH\x00R\x0eoriginalUserId\x88\x01\x01\x42\x13\n\x11_original_user_id\"\x87\x06\n\x1f\x41uthenticatePresignedURLRequest\x12$\n\x0estring_to_sign\x18\x01 \x01(\tR\x0cstringToSign\x12\x10\n\x03url\x18\x02 \x01(\tR\x03url\x12]\n\x0bhttp_method\x18\x03 \x01(\x0e\x32<.authenticator.v1.AuthenticatePresignedURLRequest.HTTPMethodR\nhttpMethod\x12*\n\x0etransaction_id\x18\x04 \x01(\tH\x00R\rtransactionId\x88\x01\x01\x12\x66\n\rx_amz_headers\x18\x05 \x03(\x0b\x32\x42.authenticator.v1.AuthenticatePresignedURLRequest.XAmzHeadersEntryR\x0bxAmzHeaders\x12o\n\x10x_akamai_headers\x18\x06 \x03(\x0b\x32\x45.authenticator.v1.AuthenticatePresignedURLRequest.XAkamaiHeadersEntryR\x0exAkamaiHeaders\x12?\n\x19skip_timestamp_validation\x18\x07 \x01(\x08H\x01R\x17skipTimestampValidation\x88\x01\x01\x1a>\n\x10XAmzHeadersEntry\x12\x10\n\x03key\x18\x01 \x01(\tR\x03key\x12\x14\n\x05value\x18\x02 \x01(\tR\x05value:\x02\x38\x01\x1a\x41\n\x13XAkamaiHeadersEntry\x12\x10\n\x03key\x18\x01 \x01(\tR\x03key\x12\x14\n\x05value\x18\x02 \x01(\tR\x05value:\x02\x38\x01\"S\n\nHTTPMethod\x12\x1b\n\x17HTTP_METHOD_UNSPECIFIED\x10\x00\x12\x13\n\x0fHTTP_METHOD_GET\x10\x01\x12\x13\n\x0fHTTP_METHOD_PUT\x10\x02\x42\x11\n\x0f_transaction_idB\x1c\n\x1a_skip_timestamp_validation\"\x7f\n AuthenticatePresignedURLResponse\x12\x17\n\x07user_id\x18\x01 \x01(\tR\x06userId\x12-\n\x10original_user_id\x18\x02 \x01(\tH\x00R\x0eoriginalUserId\x88\x01\x01\x42\x13\n\x11_original_user_id\"\x88\x01\n\x14GetSigningKeyRequest\x12\x31\n\x14\x61uthorization_header\x18\x01 \x01(\tR\x13\x61uthorizationHeader\x12*\n\x0etransaction_id\x18\x02 \x01(\tH\x00R\rtransactionId\x88\x01\x01\x42\x11\n\x0f_transaction_id\"8\n\x15GetSigningKeyResponse\x12\x1f\n\x0bsigning_key\x18\x01 \x01(\x0cR\nsigningKey2\xe7\x02\n\x14\x41uthenticatorService\x12i\n\x10\x41uthenticateREST\x12).authenticator.v1.AuthenticateRESTRequest\x1a*.authenticator.v1.AuthenticateRESTResponse\x12\x81\x01\n\x18\x41uthenticatePresignedURL\x12\x31.authenticator.v1.AuthenticatePresignedURLRequest\x1a\x32.authenticator.v1.AuthenticatePresignedURLResponse\x12`\n\rGetSigningKey\x12&.authenticator.v1.GetSigningKeyRequest\x1a\'.authenticator.v1.GetSigningKeyResponseB\xce\x01\n\x14\x63om.authenticator.v1B\x12\x41uthenticatorProtoP\x01ZAbits.linode.com/LinodeApi/obj-endpoint/gen/proto/authenticator/v1\xa2\x02\x03\x41XX\xaa\x02\x10\x41uthenticator.V1\xca\x02\x10\x41uthenticator\\V1\xe2\x02\x1c\x41uthenticator\\V1\\GPBMetadata\xea\x02\x11\x41uthenticator::V1b\x06proto3')
 
 
 
@@ -22,8 +22,11 @@ _S3ERRORDETAILS = DESCRIPTOR.message_types_by_name['S3ErrorDetails']
 _AUTHENTICATERESTREQUEST = DESCRIPTOR.message_types_by_name['AuthenticateRESTRequest']
 _AUTHENTICATERESTREQUEST_QUERYPARAMETERSENTRY = _AUTHENTICATERESTREQUEST.nested_types_by_name['QueryParametersEntry']
 _AUTHENTICATERESTREQUEST_XAMZHEADERSENTRY = _AUTHENTICATERESTREQUEST.nested_types_by_name['XAmzHeadersEntry']
+_AUTHENTICATERESTREQUEST_XAKAMAIHEADERSENTRY = _AUTHENTICATERESTREQUEST.nested_types_by_name['XAkamaiHeadersEntry']
 _AUTHENTICATERESTRESPONSE = DESCRIPTOR.message_types_by_name['AuthenticateRESTResponse']
 _AUTHENTICATEPRESIGNEDURLREQUEST = DESCRIPTOR.message_types_by_name['AuthenticatePresignedURLRequest']
+_AUTHENTICATEPRESIGNEDURLREQUEST_XAMZHEADERSENTRY = _AUTHENTICATEPRESIGNEDURLREQUEST.nested_types_by_name['XAmzHeadersEntry']
+_AUTHENTICATEPRESIGNEDURLREQUEST_XAKAMAIHEADERSENTRY = _AUTHENTICATEPRESIGNEDURLREQUEST.nested_types_by_name['XAkamaiHeadersEntry']
 _AUTHENTICATEPRESIGNEDURLRESPONSE = DESCRIPTOR.message_types_by_name['AuthenticatePresignedURLResponse']
 _GETSIGNINGKEYREQUEST = DESCRIPTOR.message_types_by_name['GetSigningKeyRequest']
 _GETSIGNINGKEYRESPONSE = DESCRIPTOR.message_types_by_name['GetSigningKeyResponse']
@@ -52,6 +55,13 @@ AuthenticateRESTRequest = _reflection.GeneratedProtocolMessageType('Authenticate
     # @@protoc_insertion_point(class_scope:authenticator.v1.AuthenticateRESTRequest.XAmzHeadersEntry)
     })
   ,
+
+  'XAkamaiHeadersEntry' : _reflection.GeneratedProtocolMessageType('XAkamaiHeadersEntry', (_message.Message,), {
+    'DESCRIPTOR' : _AUTHENTICATERESTREQUEST_XAKAMAIHEADERSENTRY,
+    '__module__' : 'authenticator.v1.authenticator_pb2'
+    # @@protoc_insertion_point(class_scope:authenticator.v1.AuthenticateRESTRequest.XAkamaiHeadersEntry)
+    })
+  ,
   'DESCRIPTOR' : _AUTHENTICATERESTREQUEST,
   '__module__' : 'authenticator.v1.authenticator_pb2'
   # @@protoc_insertion_point(class_scope:authenticator.v1.AuthenticateRESTRequest)
@@ -59,6 +69,7 @@ AuthenticateRESTRequest = _reflection.GeneratedProtocolMessageType('Authenticate
 _sym_db.RegisterMessage(AuthenticateRESTRequest)
 _sym_db.RegisterMessage(AuthenticateRESTRequest.QueryParametersEntry)
 _sym_db.RegisterMessage(AuthenticateRESTRequest.XAmzHeadersEntry)
+_sym_db.RegisterMessage(AuthenticateRESTRequest.XAkamaiHeadersEntry)
 
 AuthenticateRESTResponse = _reflection.GeneratedProtocolMessageType('AuthenticateRESTResponse', (_message.Message,), {
   'DESCRIPTOR' : _AUTHENTICATERESTRESPONSE,
@@ -68,11 +79,27 @@ AuthenticateRESTResponse = _reflection.GeneratedProtocolMessageType('Authenticat
 _sym_db.RegisterMessage(AuthenticateRESTResponse)
 
 AuthenticatePresignedURLRequest = _reflection.GeneratedProtocolMessageType('AuthenticatePresignedURLRequest', (_message.Message,), {
+
+  'XAmzHeadersEntry' : _reflection.GeneratedProtocolMessageType('XAmzHeadersEntry', (_message.Message,), {
+    'DESCRIPTOR' : _AUTHENTICATEPRESIGNEDURLREQUEST_XAMZHEADERSENTRY,
+    '__module__' : 'authenticator.v1.authenticator_pb2'
+    # @@protoc_insertion_point(class_scope:authenticator.v1.AuthenticatePresignedURLRequest.XAmzHeadersEntry)
+    })
+  ,
+
+  'XAkamaiHeadersEntry' : _reflection.GeneratedProtocolMessageType('XAkamaiHeadersEntry', (_message.Message,), {
+    'DESCRIPTOR' : _AUTHENTICATEPRESIGNEDURLREQUEST_XAKAMAIHEADERSENTRY,
+    '__module__' : 'authenticator.v1.authenticator_pb2'
+    # @@protoc_insertion_point(class_scope:authenticator.v1.AuthenticatePresignedURLRequest.XAkamaiHeadersEntry)
+    })
+  ,
   'DESCRIPTOR' : _AUTHENTICATEPRESIGNEDURLREQUEST,
   '__module__' : 'authenticator.v1.authenticator_pb2'
   # @@protoc_insertion_point(class_scope:authenticator.v1.AuthenticatePresignedURLRequest)
   })
 _sym_db.RegisterMessage(AuthenticatePresignedURLRequest)
+_sym_db.RegisterMessage(AuthenticatePresignedURLRequest.XAmzHeadersEntry)
+_sym_db.RegisterMessage(AuthenticatePresignedURLRequest.XAkamaiHeadersEntry)
 
 AuthenticatePresignedURLResponse = _reflection.GeneratedProtocolMessageType('AuthenticatePresignedURLResponse', (_message.Message,), {
   'DESCRIPTOR' : _AUTHENTICATEPRESIGNEDURLRESPONSE,
@@ -104,30 +131,42 @@ if _descriptor._USE_C_DESCRIPTORS == False:
   _AUTHENTICATERESTREQUEST_QUERYPARAMETERSENTRY._serialized_options = b'8\001'
   _AUTHENTICATERESTREQUEST_XAMZHEADERSENTRY._options = None
   _AUTHENTICATERESTREQUEST_XAMZHEADERSENTRY._serialized_options = b'8\001'
+  _AUTHENTICATERESTREQUEST_XAKAMAIHEADERSENTRY._options = None
+  _AUTHENTICATERESTREQUEST_XAKAMAIHEADERSENTRY._serialized_options = b'8\001'
+  _AUTHENTICATEPRESIGNEDURLREQUEST_XAMZHEADERSENTRY._options = None
+  _AUTHENTICATEPRESIGNEDURLREQUEST_XAMZHEADERSENTRY._serialized_options = b'8\001'
+  _AUTHENTICATEPRESIGNEDURLREQUEST_XAKAMAIHEADERSENTRY._options = None
+  _AUTHENTICATEPRESIGNEDURLREQUEST_XAKAMAIHEADERSENTRY._serialized_options = b'8\001'
   _S3ERRORDETAILS._serialized_start=59
-  _S3ERRORDETAILS._serialized_end=617
+  _S3ERRORDETAILS._serialized_end=642
   _S3ERRORDETAILS_TYPE._serialized_start=179
-  _S3ERRORDETAILS_TYPE._serialized_end=617
-  _AUTHENTICATERESTREQUEST._serialized_start=620
-  _AUTHENTICATERESTREQUEST._serialized_end=1532
-  _AUTHENTICATERESTREQUEST_QUERYPARAMETERSENTRY._serialized_start=1154
-  _AUTHENTICATERESTREQUEST_QUERYPARAMETERSENTRY._serialized_end=1220
-  _AUTHENTICATERESTREQUEST_XAMZHEADERSENTRY._serialized_start=1222
-  _AUTHENTICATERESTREQUEST_XAMZHEADERSENTRY._serialized_end=1284
-  _AUTHENTICATERESTREQUEST_HTTPMETHOD._serialized_start=1287
-  _AUTHENTICATERESTREQUEST_HTTPMETHOD._serialized_end=1438
-  _AUTHENTICATERESTRESPONSE._serialized_start=1534
-  _AUTHENTICATERESTRESPONSE._serialized_end=1653
-  _AUTHENTICATEPRESIGNEDURLREQUEST._serialized_start=1656
-  _AUTHENTICATEPRESIGNEDURLREQUEST._serialized_end=1988
-  _AUTHENTICATEPRESIGNEDURLREQUEST_HTTPMETHOD._serialized_start=1886
-  _AUTHENTICATEPRESIGNEDURLREQUEST_HTTPMETHOD._serialized_end=1969
-  _AUTHENTICATEPRESIGNEDURLRESPONSE._serialized_start=1990
-  _AUTHENTICATEPRESIGNEDURLRESPONSE._serialized_end=2117
-  _GETSIGNINGKEYREQUEST._serialized_start=2120
-  _GETSIGNINGKEYREQUEST._serialized_end=2256
-  _GETSIGNINGKEYRESPONSE._serialized_start=2258
-  _GETSIGNINGKEYRESPONSE._serialized_end=2314
-  _AUTHENTICATORSERVICE._serialized_start=2317
-  _AUTHENTICATORSERVICE._serialized_end=2676
+  _S3ERRORDETAILS_TYPE._serialized_end=642
+  _AUTHENTICATERESTREQUEST._serialized_start=645
+  _AUTHENTICATERESTREQUEST._serialized_end=1824
+  _AUTHENTICATERESTREQUEST_QUERYPARAMETERSENTRY._serialized_start=1349
+  _AUTHENTICATERESTREQUEST_QUERYPARAMETERSENTRY._serialized_end=1415
+  _AUTHENTICATERESTREQUEST_XAMZHEADERSENTRY._serialized_start=1417
+  _AUTHENTICATERESTREQUEST_XAMZHEADERSENTRY._serialized_end=1479
+  _AUTHENTICATERESTREQUEST_XAKAMAIHEADERSENTRY._serialized_start=1481
+  _AUTHENTICATERESTREQUEST_XAKAMAIHEADERSENTRY._serialized_end=1546
+  _AUTHENTICATERESTREQUEST_HTTPMETHOD._serialized_start=1549
+  _AUTHENTICATERESTREQUEST_HTTPMETHOD._serialized_end=1700
+  _AUTHENTICATERESTRESPONSE._serialized_start=1826
+  _AUTHENTICATERESTRESPONSE._serialized_end=1945
+  _AUTHENTICATEPRESIGNEDURLREQUEST._serialized_start=1948
+  _AUTHENTICATEPRESIGNEDURLREQUEST._serialized_end=2723
+  _AUTHENTICATEPRESIGNEDURLREQUEST_XAMZHEADERSENTRY._serialized_start=1417
+  _AUTHENTICATEPRESIGNEDURLREQUEST_XAMZHEADERSENTRY._serialized_end=1479
+  _AUTHENTICATEPRESIGNEDURLREQUEST_XAKAMAIHEADERSENTRY._serialized_start=1481
+  _AUTHENTICATEPRESIGNEDURLREQUEST_XAKAMAIHEADERSENTRY._serialized_end=1546
+  _AUTHENTICATEPRESIGNEDURLREQUEST_HTTPMETHOD._serialized_start=2591
+  _AUTHENTICATEPRESIGNEDURLREQUEST_HTTPMETHOD._serialized_end=2674
+  _AUTHENTICATEPRESIGNEDURLRESPONSE._serialized_start=2725
+  _AUTHENTICATEPRESIGNEDURLRESPONSE._serialized_end=2852
+  _GETSIGNINGKEYREQUEST._serialized_start=2855
+  _GETSIGNINGKEYREQUEST._serialized_end=2991
+  _GETSIGNINGKEYRESPONSE._serialized_start=2993
+  _GETSIGNINGKEYRESPONSE._serialized_end=3049
+  _AUTHENTICATORSERVICE._serialized_start=3052
+  _AUTHENTICATORSERVICE._serialized_end=3411
 # @@protoc_insertion_point(module_scope)
